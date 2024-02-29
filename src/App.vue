918f75611309c1bd69e354fcdf6b8bdf35e9c601
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-29 17:05:52 +0100

    header addedd

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+//import
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
- 
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
-<style >
-
-</style>
+<style scoped></style>
